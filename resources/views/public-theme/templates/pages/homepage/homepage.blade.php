89644a8830f805c2c3d7f493d75eb70f2627ed00
nazar <nazar.chepliaka@onix-systems.com>
2023-03-26 18:10:02 +0300

    update

--- resources/views/public-theme/templates/pages/homepage/homepage.blade.php
+++ resources/views/public-theme/templates/pages/homepage/homepage.blade.php
@@ -16,15 +16,17 @@
         <div class="row">
             @foreach($categories as $category)
                 <div data-group="categories_list" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                    <a href="{{route('categories.show',$category->id)}}" data-role="item" class="border_is_width text_center">
-                        <div data-role="item_image" class="text_center">
+                    <a href="{{route('categories.show',$category->id)}}" data-role="item" class="border_is_width flex column text_center">
+                        <div data-role="item_image" class="text_center flex_centered_item">
                             @if(!empty($category->image_path))
                                 <img src="{{$category->image_path}}">
                             @else
                                 <img src="/assets/common/images/folder-information-outline.svg">
                             @endif
                         </div>
-                        {{$category->title}}
+                        <div data-role="title" class="flex_centered_item">
+                            <span>{{$category->title}}</span>
+                        </div>
                     </a>
                 </div>
             @endforeach
